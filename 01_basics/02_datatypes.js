572424533dedb11702e7b84ab494db24a338d9aa
Yoda3101 <20210802200@dypiu.ac.in>
2024-08-31 19:30:41 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3)// we are using nodejs, not browser
+
+console.log(3 + 3); 
+// code redability  should be high 
+
+console.log("ishan")
+
+let name = "ishan" // string
+let age = 18 // number
+let isloggedIn = false // boolean
+let state;
+
+
+// number => 2 to the power 53
+// bigint for large numbers 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined);// undefined
+console.log(typeof null);// object
+
