f0fbf0905606cab9f7eb3d0b1472bdfeea036f8f
Yoda3101 <20210802200@dypiu.ac.in>
2024-08-31 19:35:09 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 144553
+let accountEmail = "ishan@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+let accountState;
+
+
+// accountId = 2 not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Beengaluru"
+
+
+console.log(accountId);
+
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
