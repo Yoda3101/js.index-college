dd18582fb63091f7ed863dbb383debc854fdfaa7
Yoda3101 <20210802200@dypiu.ac.in>
2024-09-11 13:21:55 +0000

    string

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,30 @@
+const name = "ishan"
+const repoCount = 50
+
+console.log(name + repoCount +" Value");
+
+console.log(`Hello my name is ${name} and my repo Count is ${repoCount}`);
+// node 01_basics/05_strings.js
+
+const gameName = new String('hitesh-hc-com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// 
+
+
+const url =  "https://hitesh.com/hitesh%20choudhary"
+console.log(url.replace('%20', '-'));
+
+console.log(url.includes('sundar'));
+
+console.log(gameName.split('-'));
+
+
+
+
+
+
