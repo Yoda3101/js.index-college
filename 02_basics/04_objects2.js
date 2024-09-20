d2267c8ee055297734d686d8e1c7000bb842e766
Yoda3101 <20210802200@dypiu.ac.in>
2024-09-20 19:10:41 +0000

    object part 2

---
+++ 02_basics/04_objects2.js
@@ -0,0 +1,56 @@
+// Object constructor
+
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isloggedIn = false
+// console.log(tinderUser);
+
+// node 02_basics/04_objects2.js
+
+const regularuser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname:{
+            firstname: "Ishan",
+            lastname: "Meshram"
+        }
+    }
+}
+
+// console.log(regularuser);
+
+const Obj1 = {1: "a", 2: "b"}
+const Obj2 = {3: "a", 4: "b"}
+const Obj4 = {5: "a", 6: "b"}
+
+// const obj3 = Object.assign(Obj1, Obj2, Obj4)
+// console.log(obj3);
+
+const obj3 = {...Obj1, ...Obj2, ...Obj4}
+// console.log(obj3);
+
+const users = [
+    {id: 1,
+        email: "h@gmail.com"
+    },
+    {id: 1,
+        email: "h@gmail.com"
+    },
+    {id: 1,
+        email: "h@gmail.com"
+    },
+]
+users[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+
+
+
+
+
