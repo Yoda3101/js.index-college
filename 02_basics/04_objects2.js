5fceb933b1b68bc636b4d8ba417300042649777a
Yoda3101 <20210802200@dypiu.ac.in>
2024-09-20 20:05:48 +0000

    part 2/3

--- 02_basics/04_objects2.js
+++ 02_basics/04_objects2.js
@@ -43,11 +43,22 @@ const users = [
     },
 ]
 users[1].email
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+const course ={
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+console.log(instructor);
+
 
 
 
